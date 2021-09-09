59044ca0aca0f94aab231ab2e9b8dce109584381
jordengerovac <jordengerovac@gmail.com>
2021-09-09 13:53:05 -0400

    adding spotify connect

--- src/components/ConnectSpotify.js
+++ src/components/ConnectSpotify.js
@@ -4,6 +4,8 @@ import { Redirect } from 'react-router-dom'
 import { connect } from  'react-redux';
 import NavigationBar from './NavigationBar';
 
+const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize?response_type=code&client_id=95c5f746df16436882efa3d4ebf3b9fa&redirect_uri=http://localhost:3000/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
 class ConnectSpotify extends React.Component {
     render() {
         if (!this.props.authDetails.authenticated) {
@@ -14,7 +16,8 @@ class ConnectSpotify extends React.Component {
             <div style={{height: '200vh'}}>
                 <NavigationBar />
                 <div className="App">
-                    <h2>Connect a Spotify account</h2>
+                    <h2 style={{marginBottom: '50px'}}>Connect a Spotify account</h2>
+                    <a href={SPOTIFY_AUTH_URL}><button className="bandmatesButton">Connect</button></a>
                 </div>
             </div>
         )
