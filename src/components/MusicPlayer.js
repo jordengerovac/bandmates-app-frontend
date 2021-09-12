a912f56fdb12ee24ac0551656d79565f769a0ed6
jordengerovac <jordengerovac@gmail.com>
2021-09-12 12:52:35 -0400

    some spotify player changes

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,7 +1,5 @@
 import '../App.css';
 import React from 'react';
-import { Redirect } from 'react-router-dom'
-import { connect } from  'react-redux';
 import SpotifyPlayer from 'react-spotify-web-playback';
 
 
@@ -9,5 +7,21 @@ export default function MusicPlayer(props) {
     if(!props.token){
         return null;
     }
-    return (<SpotifyPlayer play={true} token={props.token} showSaveIcon uris={props.trackUri ? [props.trackUri] : []} />)
+    return (
+        <SpotifyPlayer 
+            play={props.trackUri ? true : false} 
+            token={props.token} 
+            showSaveIcon 
+            uris={props.trackUri ? [props.trackUri] : []} 
+            styles={{
+                activeColor: '#008216',
+                bgColor: '#1b1d20',
+                color: 'white',
+                loaderColor: 'white',
+                sliderColor: '#008216',
+                trackArtistColor: 'white',
+                trackNameColor: 'white',
+            }} 
+        />
+    )
 }
--- src/components/SpotifyData.js
+++ src/components/SpotifyData.js
@@ -61,12 +61,15 @@ class SpotifyData extends React.Component {
                          <h2>Your Spotify Data</h2>
                          {this.state.spotifyData.recentTracks.map((track, i) => {
                              return(
-                                 <div>
-                                    <p onClick={() => this.setPlayingTrack(track.uri)}>{i+1}.  Artist: {track.artist}, Song Name: {track.songName}</p>
+                                <div style={{display: 'flex', justifyContent: 'center', margin: '20px auto'}}>
+                                    <img alt="album-art" onClick={() => this.setPlayingTrack(track.uri)} src={track.artwork} style={{width: '50px', cursor: 'pointer'}}/>
+                                    <p style={{cursor: 'pointer'}} onClick={() => this.setPlayingTrack(track.uri)}>{i+1}.  Artist: {track.artist}, Song Name: {track.songName}</p>
                                 </div>
                              )
                          })}
-                         <MusicPlayer token={this.state.spotifyData.spotifyAccessToken} trackUri={this.state.trackUri} />
+                         <div style={{position: 'fixed', bottom: '0', width: '100%'}}>
+                            <MusicPlayer token={this.state.spotifyData.spotifyAccessToken} trackUri={this.state.trackUri} />
+                         </div>
                     </div> : null
                 }
             </div>
